498323b509f24377c9282142889d850c2ca2e37f
t1moon <timurchik1024@gmail.com>
2017-04-11 14:18:37 +0300

    deleted module host and moved scan qr btn to Main Activity in app module

--- app/src/main/java/com/example/timur/BonusHub/activity/MainActivity.java
+++ app/src/main/java/com/example/timur/BonusHub/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.timur.BonusHub.activity;
 
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -17,6 +18,8 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.timur.BonusHub.R;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity{
 
@@ -126,6 +129,26 @@ public class MainActivity extends AppCompatActivity{
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+
+    // Get the results:
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(result != null) {
+            if(result.getContents() == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Scanned: " + result.getContents(), Toast.LENGTH_LONG).show();
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
+    public void onScanClick(View view) {
+        new IntentIntegrator(this).initiateScan();
     }
 
 }
