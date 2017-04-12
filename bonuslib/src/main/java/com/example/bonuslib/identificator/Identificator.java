d126d25152cbe238176e776dca672d6d10bb9851
romvano <romvano@gmail.com>
2017-04-12 23:27:45 +0300

    Identificator: fixed all uncommited

--- bonuslib/src/main/java/com/example/bonuslib/identificator/Identificator.java
+++ bonuslib/src/main/java/com/example/bonuslib/identificator/Identificator.java
@@ -8,36 +8,42 @@ import android.content.SharedPreferences;
  */
 
 // Singleton class for working identificator: get, set, delete
-@Singleton
 public class Identificator implements IdentificatorGetterInterface, IdentificatorSetterInterface {
-    private static Identificator instance = new Identificator(spContext);
+    private static Identificator instance;
     private SharedPreferences sp;
 
     public synchronized static Identificator getInstance(Context spContext) {
+        if (instance == null) {
+            instance = new Identificator(spContext);
+        }
         return instance;
     }
 
-    private Identificator(Context spContext, String id=null) {
-        sp = spContext.getSharedPreferences();
+    private Identificator(Context spContext) {
+        sp = spContext.getSharedPreferences(IdentificatorConstants.ID_SP_NAME, 0);
+    }
+
+    private Identificator(Context spContext, String id) {
+        sp = spContext.getSharedPreferences(IdentificatorConstants.ID_SP_NAME, 0);
         if (id != null) {
             setId(id);
         }
     }
 
     @Override
-    String getId() {
+    public String getId() {
         return sp.getString(IdentificatorConstants.ID_RECORD_NAME, null);
     }
 
     @Override
-    boolean setId(String id) {
+    public boolean setId(String id) {
         return sp.edit()
                 .putString(IdentificatorConstants.ID_RECORD_NAME, id)
                 .commit();
     }
 
-    @OnLogout
-    boolean clearId() {
+    // should be called on logout
+    public boolean clearId() {
         return sp.edit()
                 .remove(IdentificatorConstants.ID_RECORD_NAME)
                 .commit();
--- bonuslib/src/main/java/com/example/bonuslib/identificator/IdentificatorConstants.java
+++ bonuslib/src/main/java/com/example/bonuslib/identificator/IdentificatorConstants.java
@@ -4,6 +4,7 @@ package com.example.bonuslib.identificator;
  * Created by ivan on 4/12/17.
  */
 
-public final static class IdentificatorConstants {
-    public static String ID_RECORD_NAME = "identificator";
+public final class IdentificatorConstants {
+    public final static String ID_RECORD_NAME = "identificator";
+    public final static String ID_SP_NAME = "identificator_shared_preferences";
 }
