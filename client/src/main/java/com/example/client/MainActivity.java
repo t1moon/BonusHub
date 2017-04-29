253d188cd27deedb9cc39ef60175a51bdb4f5083
t1moon <timurchik1024@gmail.com>
2017-04-29 15:22:36 +0300

    added common code to lib

--- client/src/main/java/com/example/client/MainActivity.java
+++ client/src/main/java/com/example/client/MainActivity.java
@@ -1,13 +1,140 @@
 package com.example.client;
 
+import android.content.res.Configuration;
+import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.example.client.fragment.ListHostFragment;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawer;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+
+        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawerToggle = new ActionBarDrawerToggle(this, mDrawer, mToolbar, R.string.drawer_open, R.string.drawer_close);
+        mDrawer.addDrawerListener(drawerToggle);
+        nvDrawer = (NavigationView) findViewById(R.id.navigation_view);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
+        // Setup behaviour for back home button, hamburger etc.
+        getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
+                                                                      @Override
+                                                                      public void onBackStackChanged() {
+                                                                          if (getSupportFragmentManager().getBackStackEntryCount() > 0) {
+                                                                              getSupportActionBar().setDisplayHomeAsUpEnabled(true); // show back button
+                                                                              mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                                                                                  @Override
+                                                                                  public void onClick(View v) {
+                                                                                      onBackPressed();
+                                                                                  }
+                                                                              });
+                                                                          } else {
+                                                                              //show hamburger
+                                                                              getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+                                                                              drawerToggle.syncState();
+                                                                              mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+                                                                                  @Override
+                                                                                  public void onClick(View v) {
+                                                                                      mDrawer.openDrawer(GravityCompat.START);
+                                                                                  }
+                                                                              });
+                                                                          }
+                                                                      }
+                                                                  }
+        );
+        setupStartFragment();
     }
+
+    private void setupStartFragment() {
+        Fragment fragment = null;
+        int host_id = this.getPreferences(MODE_PRIVATE).getInt("host_id", -1);
+        try {
+            fragment = (Fragment) ListHostFragment.class.newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        }
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container_body, fragment).commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggles
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass = null;
+        switch (menuItem.getItemId()) {
+            case R.id.nav_second_fragment:
+                fragmentClass = ListHostFragment.class;
+                break;
+        }
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.container_body, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
+    }
+
+
+
 }
---
+++ client/src/main/java/com/example/client/fragment/ListHostFragment.java
@@ -0,0 +1,52 @@
+package com.example.client.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.bonuslib.db.HelperFactory;
+import com.example.bonuslib.host.Host;
+import com.example.client.R;
+
+
+import java.sql.SQLException;
+
+import static android.content.Context.MODE_PRIVATE;
+
+public class ListHostFragment extends Fragment {
+
+    public ListHostFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_list_host, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
