3398abc8362b8e6bf8a18dbee5aedb199c08f6da
t1moon <timurchik1024@gmail.com>
2017-04-29 16:23:05 +0300

    client added recyclerview

---
+++ client/src/main/java/com/example/client/HostAdapter.java
@@ -0,0 +1,54 @@
+package com.example.client;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.example.bonuslib.host.Host;
+
+import java.util.List;
+
+/**
+ * Created by Timur on 29-Apr-17.
+ */
+
+public class HostAdapter extends RecyclerView.Adapter<HostAdapter.MyViewHolder>  {
+
+    private List<Host> hostsList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.rv_title);
+        }
+    }
+
+
+    public HostAdapter(List<Host> hostsList) {
+        this.hostsList = hostsList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.host_list_row, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Host host = hostsList.get(position);
+        holder.title.setText(host.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return hostsList.size();
+    }
+}
--- client/src/main/java/com/example/client/fragment/ListHostFragment.java
+++ client/src/main/java/com/example/client/fragment/ListHostFragment.java
@@ -3,6 +3,9 @@ package com.example.client.fragment;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,14 +14,21 @@ import android.widget.TextView;
 
 import com.example.bonuslib.db.HelperFactory;
 import com.example.bonuslib.host.Host;
+import com.example.client.HostAdapter;
 import com.example.client.R;
 
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.content.Context.MODE_PRIVATE;
 
 public class ListHostFragment extends Fragment {
+
+    private List<Host> hostList = new ArrayList<>();
+    private RecyclerView recyclerView;
+    private HostAdapter mAdapter;
 
     public ListHostFragment() {
         // Required empty public constructor
@@ -36,7 +46,27 @@ public class ListHostFragment extends Fragment {
         // Inflate the layout for this fragment
         final View rootView = inflater.inflate(R.layout.fragment_list_host, container, false);
 
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.recycler_view);
+        mAdapter = new HostAdapter(hostList);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        prepareHostData();
+
         return rootView;
+    }
+
+    private void prepareHostData() {
+        Host host = new Host("first", "desc");
+        hostList.add(host);
+
+        host = new Host("se", "qwe");
+        hostList.add(host);
+
+        mAdapter.notifyDataSetChanged();
+
     }
 
     @Override
