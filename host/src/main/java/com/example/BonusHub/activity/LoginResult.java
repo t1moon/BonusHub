bc1bbd71b1c382d4334fcd8c4c7c0c23b4f43cdd
burmistrovm <burmistrowm@mail.ru>
2017-05-05 13:49:42 +0300

    Загрузка данных из сети.

---
+++ host/src/main/java/com/example/BonusHub/activity/LoginResult.java
@@ -0,0 +1,19 @@
+package com.example.BonusHub.activity;
+
+/**
+ * Created by mike on 16.04.17.
+ */
+
+public class LoginResult {
+    private int code;
+
+    private String message;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
