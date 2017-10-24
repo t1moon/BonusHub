25c6204470b0b12207ecf0ebfe4c21e0abf89266
romvano <romvano@gmail.com>
2017-10-24 21:53:34 +0300

    owner settings except first open init from server

--- bonushub/src/main/java/com/example/BonusHub/fragment/OwnerSettingsFragment.java
+++ bonushub/src/main/java/com/example/BonusHub/fragment/OwnerSettingsFragment.java
@@ -2,16 +2,20 @@ package com.example.BonusHub.fragment;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
 import android.support.v7.preference.PreferenceManager;
 import android.util.Log;
 
 import com.example.BonusHub.activity.HostMainActivity;
+import com.example.BonusHub.db.host.Host;
 import com.example.BonusHub.retrofit.HostApiInterface;
 import com.example.BonusHub.retrofit.RetrofitFactory;
+import com.example.BonusHub.retrofit.getInfo.GetInfoResponse;
 import com.example.BonusHub.retrofit.loyality.EditLoyalityRequest;
 import com.example.BonusHub.retrofit.loyality.EditLoyalityResponse;
 import com.example.BonusHub.utils.AuthUtils;
@@ -19,7 +23,11 @@ import com.example.BonusHub.utils.NumberPreference.NumberPickerPreferenceDialog;
 import com.example.BonusHub.utils.NumberPreference.NumberPreference;
 import com.example.timur.BonusHub.R;
 
+import java.util.Arrays;
+
 import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -31,17 +39,22 @@ import retrofit2.Call;
  * create an instance of this fragment.
  */
 public class OwnerSettingsFragment extends PreferenceFragmentCompat implements SharedPreferences.OnSharedPreferenceChangeListener {
+    private boolean resumed = false;
     public final static String TAG = "OwnerSettingsFragment";
     private SharedPreferences sp;
     protected final String THIS_FRAGMENT = "android.support.v7.preference.PreferenceFragment.DIALOG";
     protected final int BONUS_SYSTEM_KEY_ADDRESS = R.string.bonus_system_key;
-    protected final int TYPED_SETTINGS_KEY = R.string.typed_settings_key;
+    protected final int TYPED_SETTINGS_KEYS[] = {
+            R.string.cup_param_key,
+            R.string.percent_param_key
+    };
     protected final int TYPED_SETTINGS[] = {
             R.xml.xml_cup_settings,
             R.xml.xml_percent_settings//,
 //            R.xml.xml_total_discout_settings
     };
-    protected int bst = -1; // bonus system type
+    protected int bst = 1; // bonus system type (before update)
+    protected int bsp = 10; // bonus system param (before update)
     HostApiInterface hostApiInterface;
 
     public OwnerSettingsFragment() {
@@ -57,50 +70,77 @@ public class OwnerSettingsFragment extends PreferenceFragmentCompat implements S
         // register listener in order to change settings appearence depending
         // on bonus system type
         sp.registerOnSharedPreferenceChangeListener(this);
-        // find preferences for chosen bonus type
-
-        // retrifit init
+        // retrofit init
         hostApiInterface = RetrofitFactory.retrofitHost().create(HostApiInterface.class);
+
+//        final Call<GetInfoResponse> call =
+//                hostApiInterface.getInfo(AuthUtils.getCookie(((HostMainActivity) getActivity()).getApplicationContext()));
+//        call.enqueue(new Callback<Host>() {
     }
 
     // register listener for bonus type change
     @Override
     public void onResume() {
         super.onResume();
+        resumed = true;
         sp.registerOnSharedPreferenceChangeListener(this);
         // update bonus system type
         bst = Integer.parseInt(sp.getString(getString(BONUS_SYSTEM_KEY_ADDRESS), "1"));
+        bsp = (int) sp.getInt(getString(TYPED_SETTINGS_KEYS[bst]), 10);
         render();
     }
 
     @Override
     public void onPause() {
         super.onPause();
+        resumed = false;
         sp.unregisterOnSharedPreferenceChangeListener(this);
     }
 
     // list of actions perfomed depending on a certain preference changed
     @Override
-    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+    public void onSharedPreferenceChanged(final SharedPreferences sharedPreferences, String key) {
         // this if in order to use getString()
         if (!isAdded()) {
             Log.d(TAG, "onSharedPreferenceChanged: not added to activity");
             return;
         }
-
-        if (getString(BONUS_SYSTEM_KEY_ADDRESS).equals(key)) {
-            int bonusSystemType = Integer.parseInt(sharedPreferences.getString(key, ""));
-            bst = bonusSystemType;
-
-            render();
-        }
-
+        // if any preference changed:
         if (getString(BONUS_SYSTEM_KEY_ADDRESS).equals(key) ||
-                getString(TYPED_SETTINGS_KEY).equals(key)) {
-            int loyalityParam = Integer.parseInt(sharedPreferences.getString(getString(TYPED_SETTINGS_KEY), "10"));
+                getString(TYPED_SETTINGS_KEYS[0]).equals(key) ||
+                getString(TYPED_SETTINGS_KEYS[1]).equals(key)) {
+            // take loyality values from sp
+            final int loyalityType = Integer.parseInt(sharedPreferences.getString(
+                    getString(BONUS_SYSTEM_KEY_ADDRESS), "1"));
+            final int loyalityParam = (int) sharedPreferences.getInt(
+                    getString(TYPED_SETTINGS_KEYS[loyalityType]), 10);
+            // make a request with them
             final Call<EditLoyalityResponse> call =
                     hostApiInterface.editLoyality(new EditLoyalityRequest(bst, loyalityParam, ""),
                     AuthUtils.getCookie(((HostMainActivity) getActivity()).getApplicationContext()));
+            // success => ok, failure => we need to reset values
+            call.enqueue(new Callback<EditLoyalityResponse>() {
+                @Override
+                public void onResponse(Call<EditLoyalityResponse> call, Response<EditLoyalityResponse> response) {
+                    Log.d(TAG, "onResponse: " + response.body().getCode());
+                    bst = loyalityType;
+                    bsp = loyalityParam;
+                    render();
+                    Snackbar snackbar = Snackbar.make(getView(), getString(R.string.loyality_changed), Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                }
+
+                @Override
+                public void onFailure(Call<EditLoyalityResponse> call, Throwable t) {
+                    Snackbar snackbar = Snackbar.make(getView(), getString(R.string.loyality_change_failed), Snackbar.LENGTH_LONG);
+                    snackbar.show();
+                    if (resumed) sp.unregisterOnSharedPreferenceChangeListener(OwnerSettingsFragment.this);
+                    // change back
+                    ((ListPreference) findPreference(getString(BONUS_SYSTEM_KEY_ADDRESS))).setValueIndex(bst);
+                    ((NumberPreference) findPreference(getString(TYPED_SETTINGS_KEYS[bst]))).setValue(bsp);
+                    if (resumed) sp.registerOnSharedPreferenceChangeListener(OwnerSettingsFragment.this);
+                }
+            });
         }
     }
 
