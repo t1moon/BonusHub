e9fe0d8ebac633e3966b7ae88ba9ca5f6d5b5b09
t1moon <timurchik1024@gmail.com>
2017-12-04 14:15:14 +0300

    Сделал поддержку старых устройств в bonus hub ещё раз 2

--- bonushub/src/main/java/com/example/BonusHub/fragment/ProfileFragment.java
+++ bonushub/src/main/java/com/example/BonusHub/fragment/ProfileFragment.java
@@ -4,12 +4,14 @@ import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.res.ResourcesCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AlertDialog;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -102,7 +104,8 @@ public class ProfileFragment extends Fragment {
         host_open_time_tv = (TextView) rootView.findViewById(R.id.host_open_time_tv);
         host_close_time_tv = (TextView) rootView.findViewById(R.id.host_close_time_tv);
         fab_edit = (FloatingActionButton) hostMainActivity.findViewById(R.id.fab);
-        fab_edit.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.ic_edit_black_24dp, null));
+        Drawable drawable = DrawableCompat.wrap(ContextCompat.getDrawable(getActivity(), R.drawable.ic_edit_black_24dp));
+        fab_edit.setImageDrawable(drawable);
         fab_edit.setVisibility(View.VISIBLE);
 
         fab_edit.setOnClickListener(new View.OnClickListener() {
